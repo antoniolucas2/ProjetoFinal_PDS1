6086976e9f39f4604a48f77720d884314137ad9b
antoniolucas2 <antoniolucas_sales@outlook.com>
2025-06-12 14:18:09 -0300

    Adicionei a pasta dos includes

---
+++ include/initialize.h
@@ -0,0 +1,9 @@
+#ifndef INITIALIZE_HEADER
+#define INITIALIZE_HEADER
+
+/* Essa funcao eh responsavel por iniciar tudo que eh necessario para o Allegro,
+ * e caso algo nao consiga ser inicializado, ela imprime no stderr o que nao foi
+ * inicializado e termina o programa com exit(CODIGO) */
+void initialize_allegro();
+
+#endif
