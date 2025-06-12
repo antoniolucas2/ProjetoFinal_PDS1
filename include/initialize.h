27d1904e81553bf79de384bbab4c89b16e05a69e
antoniolucas2 <antoniolucas_sales@outlook.com>
2025-06-12 14:24:40 -0300

    Documentacao das funcoes

--- include/initialize.h
+++ include/initialize.h
@@ -3,7 +3,8 @@
 
 /* Essa funcao eh responsavel por iniciar tudo que eh necessario para o Allegro,
  * e caso algo nao consiga ser inicializado, ela imprime no stderr o que nao foi
- * inicializado e termina o programa com exit(CODIGO) */
+ * inicializado e termina o programa com exit(CODIGO). Esses codigos estao definidos
+ * no header constants.h*/
 void initialize_allegro();
 
 #endif
