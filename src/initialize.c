1e52fd24ebda2ea349279feea6809a81321a636b
antoniolucas2 <antoniolucas_sales@outlook.com>
2025-06-12 14:41:41 -0300

    Adicionei mais algumas coisas no initialize

--- src/initialize.c
+++ src/initialize.c
@@ -3,19 +3,22 @@
 #include <stdbool.h>
 #include <stdio.h>
 
+#include "initialize.h" 
+#include "constants.h"
+
 void initialize_allegro(){
 
   if(!al_init()){
 
     fprintf(stderr, "Nao consegui iniciar o Allegro!\n");
-    exit(1);
+    exit(ALLEGRO_NAO_INICIOU);
 
   }
 
   if(!al_install_keyboard()){
 
     fprintf(stderr, "Nao consegui instalar o teclado!\n");
-    exit(1);
+    exit(TECLADO_NAO_FOI_INSTALADO);
 
   }
 
